35533561eaa184b0df6212b545a87cce2c9ef415
KarlaG09 <kg02793@srcs.org>
2021-09-21 14:45:05 -0700

    Day 1: Starting

--- Level1.java
+++ Level1.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class BrickWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Karla
+ * @version 9/21/21
  */
 public class Level1 extends World
 {
@@ -16,5 +16,32 @@ public class Level1 extends World
     {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1, false); 
+        prepare();
+        setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectible.class,
+              Door.class, HUD.class);
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Player player = new Player();
+        addObject(player,38,755);
+        Door door = new Door();
+        addObject(door,1175,128);
+        Player player2 = new Player();
+        addObject(player2,92,748);
+        player.setLocation(67,748);
+        player.setLocation(71,741);
+        player.setLocation(55,738);
+        player.setLocation(63,741);
+        player.setLocation(199,722);
+        player.setLocation(443,500);
+        player.setLocation(435,501);
+        player.setLocation(-72,152);
+        removeObject(player);
+        player2.setLocation(40,728);
     }
 }
