5613122b0fa3b9d00264b3744882c30d40af157d
KarlaG09 <kg02793@srcs.org>
2021-09-27 15:48:45 -0700

    Activity 3

--- Level1.java
+++ Level1.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Level1 extends World
 {
+    private final float GRAVITY = 0.0667f;
+    private final GreenfootSound MUSIC = new GreenfootSound("zapsplat_024.mp3");
     /**
      * Constructor for objects of class BrickWorld.
      * 
@@ -43,5 +45,27 @@ public class Level1 extends World
         player.setLocation(-72,152);
         removeObject(player);
         player2.setLocation(40,728);
+        addObject(new Floor(), 600, 800);
+        addObject(new BrickWall(), 880, 650);
+        addObject(new SmBrickWall(), 250, 550);
+        addObject(new BrickWall(), 550, 450);
+        addObject(new SmBrickWall(), 1100, 300);
+        addObject(new BrickWall(), 650, 200);
+        addObject(new SmBrickWall(), 1150, 185);
+        addObject(new Bomb(GRAVITY), 850, 165);
+        addObject(new Bomb(GRAVITY), 1050, 770);
+        addObject(new Powerup(), 43, 56);
+        addObject(new TrapDoor(GRAVITY), 200, 550);
+        addObject(new TrapDoor(GRAVITY), 950, 650);
+        addObject(new Powerup(), 1050, 270);
+        addObject(new Collectible(), 1125, 265);
+    }
+    
+    private void spawn()
+    {
+        if(Math.random() < 0.0025)
+        {
+            addObject(new Rock(GRAVITY), Greenfoot.getRandomNumber(1200),   -30);
+        }
     }
 }
