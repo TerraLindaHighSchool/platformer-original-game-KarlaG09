167eea69edd69b3b333b18e1163f4b8857503c7d
KarlaG09 <kg02793@srcs.org>
2021-10-21 15:41:17 -0700

    10/21/21

--- Level2.java
+++ Level2.java
@@ -3,29 +3,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class BrickWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Karla
+ * @version 9/21/21
  */
 public class Level2 extends World
 {
-      private final float GRAVITY = 0.0667f;
-    private final GreenfootSound MUSIC = new GreenfootSound("incompetech_tribal.mp3");
+    private final float GRAVITY = 0.0667f;
+    private final GreenfootSound MUSIC = new GreenfootSound("zapsplat_024.mp3");
     private final int SPEED = 3;
-    private final float JUMP_FORCE = 5.6f;
+    private final float JUMP_FORCE = 5.8f;
     private final int MAX_HEALTH = 3;
     private final int MAX_POWERUP = 3;
-    private final Class NEXT_LEVEL = Level3.class;
+    private final Class NEXT_LEVEL = Level2.class;
+    
     /**
      * Constructor for objects of class BrickWorld.
      * 
      */
+
     public Level2()
     {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
-        super(1200, 800, 1); 
-        prepare();
+        super(1200, 800, 1, false); 
+        prepare();  
     }
-
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
     private void prepare()
     { 
         setPaintOrder(Player1.class, Platform.class, Obstacle.class, Collectible.class, Door.class, HUD.class);
@@ -43,11 +49,35 @@ public class Level2 extends World
         player.setLocation(-72,152);
         removeObject(player);
         addObject(new Floor2(), 600, 800);
-
+        addObject(new SmBrickWall(),150 , 650);
+        addObject(new SmBrickWall(), 1025 , 650 );
+        addObject(new BrickWall(), 550 , 550 );
+        addObject(new SmBrickWall(), 225, 400);
+        addObject(new SmBrickWall(),500 , 300);
+        addObject(new SmBrickWall(),750 , 350);
+        addObject(new SmBrickWall(),950 , 250);
+        addObject(new SmBrickWall(),200, 150);
+        addObject(new BrickWall(), 600, 50);
+        addObject(new SmBrickWall(), 1150, 185);
+        addObject(new Bomb(GRAVITY), 850, 165);
+        addObject(new Bomb(GRAVITY), 1050, 770);
+        addObject(new Powerup(), 43, 56);
+        addObject(new TrapDoor(GRAVITY), 200, 550);
+        addObject(new TrapDoor(GRAVITY), 950, 650);
         addObject(new Powerup(), 1050, 270);
         addObject(new Collectible(), 1125, 265);
         Player1 player1 = new Player1(3, 5.6f, GRAVITY, 3, 3, Level2.class, MUSIC);
         addObject(player1,34,760);
         player1.setLocation(54,690);
     }
+    
+    private void spawn()
+    { 
+
+        if(Math.random() < 0.0025)
+        {
+            addObject(new Rock(GRAVITY), Greenfoot.getRandomNumber(1200),   -30);
+        }
+    }
+       
 }
